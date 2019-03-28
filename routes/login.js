0d4f9207d7701d7ce3109738364c9bc38a645937
johnaq <863339@TCS.com>
2019-03-28 13:25:55 -0500

    Diseño de login

--- routes/login.js
+++ routes/login.js
@@ -27,38 +27,6 @@ router.post('/', function(req, res, next) {
     }
 });
 
-// /* Nuevo usuario */
-// router.get('/nuevo', function(req, res, next) {
-//   res.render('usuarios/nuevo',
-//   {
-//       title: "Crear usuario"
-//   });
-// });
-
-// /* Nuevo curso */
-// router.post('/nuevo', function(req, res, next) {
-//     cargarArchivo();
-//     let buscar = usuarios.find(x => x.docUsuario == req.body.docUsuario);
-
-//     if(buscar === undefined){
-//         let usuario = {
-//             docUsuario:         req.body.docUsuario,
-//             nombreUsuario:      req.body.nombreUsuario,
-//             emailUsuario:       req.body.emailUsuario,
-//             telUsuario:         req.body.telUsuario,
-//             pswUsuario:         req.body.pswUsuario,
-//             pswConfirmaUsuario: req.body.pswConfirmaUsuario,
-//             rolUsuario:         'Aspirante'
-//         };
-//         usuarios.push(usuario);
-//         guardarArchivo(JSON.stringify(usuarios));
-//         req.flash('mensajeExito', 'Usuario creado correctamente')
-//     }else{
-//         req.flash('mensajeError', 'El usuario con identificación '+req.body.docUsuario+' ya existe')
-//     }
-//     res.redirect('/usuarios/nuevo')
-// });
-
 let cargarArchivo = () => {
     try{
         let data = fs.readFileSync(archivo)
