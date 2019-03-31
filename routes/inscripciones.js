b8b263c336e31f0906567a984575168b97c8c21a
Jhon Alzate <jhonalexanderalzatemacea@gmail.com>
2019-03-30 23:43:54 -0500

    Se adiciona el listar inscripciones para los asppirantes y se organizan los enlaces de navegación del menú para cursos e inscripciones

--- routes/inscripciones.js
+++ routes/inscripciones.js
@@ -11,8 +11,14 @@ var cursos = [];
 
 /* Nueva inscripción*/
 router.get('/nuevo/:idCurso', function(req, res, next) {
+
+    if(!req.session.usuario){
+        req.flash('mensajeError', 'No tiene permisos')
+        res.redirect('/')
+        return;
+    }
+    let session = req.session.usuario;    
     cargarArchivo();
-    let session = req.session.usuario;
     //Se buscan los cursos de la persona que se encuentre logueada
     let buscar = inscripciones.find(x => x.docUsuario == session.docUsuario & x.idCurso == req.params.idCurso);cargarCursos();
     let curso = cursos.find(x => x.idCurso == req.params.idCurso);
@@ -34,24 +40,33 @@ router.get('/nuevo/:idCurso', function(req, res, next) {
     }else{
         req.flash('mensajeError', 'Ya se encuentra inscrito en el curso ' + curso.nombreCurso)
     }
-    res.redirect('/cursos/'+req.params.idCurso)
+    res.redirect('/inscripciones')
 });
 
-// /* Ver curso */
-// router.get('/:id', function(req, res, next) {
-//     cargarArchivo();
-//     let buscar = cursos.find(x => x.idCurso == req.params.id);
-//     if(buscar !== undefined){
-//         res.render('cursos/curso', 
-//         { 
-//             title: 'Cursos - ' + buscar.nombreCurso,
-//             curso: buscar
-//         });
-//     }else{req
+/* Ver inscripciones */
+router.get('/', function(req, res, next) {
+    if(!req.session.usuario){
+        req.flash('mensajeError', 'No tiene permisos')
+        res.redirect('/')
+        return;
+    }
+    let session = req.session.usuario;
+    cargarArchivo();
+    let buscar = inscripciones.filter(x => x.docUsuario == session.docUsuario);
+    // console.log(buscar);    
+    if(buscar !== undefined){
+        res.render('inscripciones/index',
+        { 
+            title: 'Inscripciones - ' + session.nombreUsuario,
+            inscripcion: buscar,
+            coordinador: (session.rolUsuario == 'Coordinador') ? true : false,
+            aspirante: (session.rolUsuario == 'Aspirante') ? true : false
+        });
+    }else{
         
-//         res.redirect('/cursos')
-//     }
-// });
+        res.redirect('/cursos')
+    }
+});
 
 let cargarArchivo = () => {
     try{
