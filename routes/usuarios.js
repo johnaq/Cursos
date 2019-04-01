aebe6d7f84b70a66e3a140e9364301061448ac50
jhonal28 <jalzate0128@gmail.com>
2019-03-31 22:59:11 -0500

    Se adiciona módulo para actualizar los datos generales del usuario y se hacen ajustes al menú de la aplicación

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,9 +6,25 @@ var router = express.Router();
 const archivo = path.join(__dirname , '../datos/usuarios.json');
 var usuarios = [];
 
-// router.get('/', function(req, res, next) {
-//     res.render('index', { title: 'Usuarios' });
-// });
+//Lista de usuarios
+router.get('/', function(req, res, next) {
+    let session = req.session.usuario;
+    if(session.rolUsuario != 'Coordinador'){
+        req.flash('mensajeError', 'No tiene permisos')
+        res.redirect('/cursos')
+        return;
+    }
+    
+    cargarArchivo();
+    
+    res.render('usuarios/index', 
+    { 
+        title: 'Usuarios',
+        listaUsuarios: usuarios,
+        coordinador: (session.rolUsuario == 'Coordinador') ? true : false,
+        aspirante: (session.rolUsuario == 'Aspirante') ? true : false 
+    });
+});
 
 /* Nuevo usuario */
 router.get('/nuevo', function(req, res, next) {
@@ -40,6 +56,40 @@ router.post('/nuevo', function(req, res, next) {
         req.flash('mensajeError', 'El usuario con identificación '+req.body.docUsuario+' ya existe')
     }
     res.redirect('/usuarios/nuevo')
+});
+
+router.post('/editar', function(req, res, next) {
+    cargarArchivo();
+    let session = req.session.usuario;
+    let buscar = usuarios.find(x => x.docUsuario == req.body.docUsuario);
+    if(buscar !== undefined){
+        
+        buscar['nombreUsuario'] = req.body.nombreUsuario;
+        buscar['emailUsuario'] = req.body.emailUsuario;
+        buscar['telUsuario'] = req.body.telUsuario;
+        buscar['rolUsuario'] = (req.body.newRolUsuario == '0') ? buscar.rolUsuario:req.body.newRolUsuario;
+        guardarArchivo(JSON.stringify(usuarios))
+        req.flash('mensajeExito', 'Usuario editado con exito')
+        res.redirect(req.get('referer'))
+    }else{
+        req.flash('mensajeError', 'No se pudo editar el usuario')
+        res.redirect('/usuarios')
+    }
+});
+
+/* editar usuarios */
+router.get('/editar/:docUsuario', function(req, res, next) {
+    cargarArchivo();
+    let session = req.session.usuario;
+    let buscar = usuarios.find(x => x.docUsuario == req.params.docUsuario);
+
+    res.render('usuarios/editar', 
+    { 
+        title: 'Editar - ' + buscar.nombreUsuario,
+        usuario: buscar,
+        coordinador: (session.rolUsuario == 'Coordinador') ? true : false,
+        aspirante: (session.rolUsuario == 'Aspirante') ? true : false 
+    });
 });
 
 let cargarArchivo = () => {
